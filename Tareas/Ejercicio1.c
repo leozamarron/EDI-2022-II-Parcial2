ca91a10c31e44e5979f7edd460ea9d8e1cc2c098
Leonel Zamarron <zamarronleo4@gmail.com>
2022-03-14 23:55:14 -0600

    14 de marzo

---
+++ Tareas/Ejercicio1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#define N 20
+
+int recursividad(char *cadena, int inicio, int fin);
+void cambioAlRevez(char *copia, int fin);
+void eliminaEspacios(char *cadena, int *fin);
+
+int main()
+{
+    char cadena[N];
+    int inicio = 0;
+    int fin;
+    int palindroma;
+
+    printf("Dame la cadena: ");
+    scanf("%[^\n]", &cadena);
+
+    fin = strlen(cadena);
+    eliminaEspacios(cadena, &fin);
+
+    palindroma = recursividad(cadena, inicio, fin);
+    printf("%d", palindroma);
+
+    return 0;
+}
+
+void eliminaEspacios(char *cadena, int *fin)
+{
+    for (int i = 0; cadena[i] != '\0'; i++)
+    {
+        if (cadena[i] == ' ')
+        {
+            for (int j = i; j < *fin; j++)
+            {
+                cadena[j] = cadena[j+1];
+            }
+            *fin = *fin - 1;
+        }
+        
+    }
+}
+
+void cambioAlRevez(char *copia, int fin)
+{
+    int aux;
+
+    for (int j = fin; j > 0; j--)
+    {
+        for (int i = 0; i < fin; i++)
+        {
+            aux = copia[j];
+            copia[j] = copia[i];
+            copia[i] = copia[j];
+        }
+    }
+}
+
+int recursividad(char *cadena, int inicio, int fin)
+{
+    char copia[N];
+    strcpy(copia, cadena);
+    cambioAlRevez(copia, fin);
+
+    if (inicio > fin)
+    {
+        return 1;
+    }
+    if (cadena[inicio] == copia [fin])
+    {
+        return recursividad(cadena, inicio + 1, fin - 1);
+    }
+    else
+    {
+        return 0;
+    }
+}
